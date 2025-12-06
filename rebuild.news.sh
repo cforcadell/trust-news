a7dbd8614b476170d31c272307a913093f51cd10
Carles Forcadell <cforcadell@gmail.com>
2025-12-06 15:30:59 +0100

    Eliminarmos secrets de cmpose blockchain

--- rebuild.news.sh
+++
@@ -1,3 +0,0 @@
-docker compose  -f docker-compose.apis.yml down 'news-handler'
-docker compose  -f docker-compose.apis.yml build 'news-handler'
-docker compose  -f docker-compose.apis.yml up 'news-handler'

0f2423d40da71096a8768ca08c87e9f945302a33
Carles Forcadell <cforcadell@gmail.com>
2025-12-07 18:49:51 +0100

    Restrucruación de folders

--- rebuild.base.sh
+++
@@ -1,3 +0,0 @@
-docker compose  -f docker-compose.base.yml down 
-docker compose  -f docker-compose.base.yml build 
-docker compose  -f docker-compose.base.yml up 
--- stop_containers.sh
+++
@@ -1,3 +0,0 @@
-docker compose  -f docker-compose.apis.yml down 
-docker compose  -f docker-compose.base.yml down 
-docker compose  -f docker-compose.blockchain.yml down 

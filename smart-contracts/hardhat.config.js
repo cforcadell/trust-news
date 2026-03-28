14a2a6a66e6544d082f1c34cbae2c802eca9c756
cforcadell <96940549+cforcadell@users.noreply.github.com>
2026-03-28 20:01:34 +0100

    Remove private account key from hardhat config
    Removed a private account key from the configuration.

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -13,7 +13,7 @@ module.exports = {
       url: "http://192.168.56.108:8545", // el port HTTP del node N1
       accounts: [
         //"0x<CLAU_PRIVADA_DEL_COMPTE_QUE_HAS_DESBLOQUEJAT>"
-        "45a407904ebd8d0f6e9690ee36d7654c50a20e43dc2fa863c1eea9633f567667"
+        ""
       ],
       chainId: 12345
     }
